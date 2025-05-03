96e7b971981c19283545a882297e49324f110a40
sankalpaacharya <sankalpaacharya01@gmail.com>
2025-05-03 15:24:26 +0530

    feat(design): add project url

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -11,12 +11,14 @@ export default function Projects() {
       <div className="space-y-3 flex gap-5 flex-col mt-5">
         <ProjectCard
           title="Weride.live"
+          projectUrl="https://weride.live"
           description="Making vehicle sharing easier and more comfortable for hostel students."
           imageUrl="/images/weride.png"
           buttonLink="askldj"
           buttonText="Website"
         />
         <ProjectCard
+          projectUrl="https://repo-deleter.vercel.app/"
           title="Cleaner"
           description="An webapp to clean github repository so that you dont have to manually delete all of them"
           imageUrl="https://www.sankalpa.info.np/_astro/repo-deleter.Dq60gM2z_miB0p.webp"
