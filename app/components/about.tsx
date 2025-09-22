a1f28a91a47f137dda2eb7e224d30b5ef32a1489
sankalpaacharya <sankalpaacharya01@gmail.com>
2025-09-23 02:19:55 +0530

    feat: little responsive

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -191,7 +191,7 @@ export default function About() {
           try.
         </p>
       </motion.div>
-      <div className="mt-5 flex gap-3">
+      <div className="mt-5 flex gap-3 flex-col md:flex-row">
         <div className="text-xs bg-card px-4 py-2 border flex rounded-md cursor-pointer items-center gap-2">
           <span className="relative flex size-3">
             {" "}
