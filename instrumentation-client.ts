cdc66ef98516c271e6964d3461ecbf046af22884
sankalpaacharya <sankalpaacharya01@gmail.com>
2025-11-19 11:38:43 +0530

    chore: posthog capture pageview

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -4,6 +4,7 @@ posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: "/ingest",
     ui_host: "https://us.posthog.com",
     defaults: '2025-05-24',
-    capture_exceptions: true, // This enables capturing exceptions using Error Tracking
+    capture_exceptions: true,
+    capture_pageview: true,
     debug: process.env.NODE_ENV === "development",
 });
